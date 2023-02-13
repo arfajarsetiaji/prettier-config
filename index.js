12506629474e1de1964dd26d9ec96af6c9c614d8
Ar Fajar Setiaji <arfajarsetiaji.id@gmail.com>
2023-02-13 19:33:56 +0700

    feat: update prettier config in index.js & .prettierrc.js

--- index.js
+++ index.js
@@ -21,4 +21,10 @@ module.exports = {
   requirePragma: false,
   rangeStart: 0,
   rangeEnd: Infinity,
+  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderGroupNamespaceSpecifiers: false,
+  importOrderCaseInsensitive: false,
+  importOrderParserPlugins: ["typescript", "jsx"],
 };
